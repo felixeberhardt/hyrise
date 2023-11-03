1df07d6e3059e5032647e5aa6bdac0532942e6e8
Felix Eberhardt <felix.eberhardt@hpi.de>
2023-11-03 09:54:24 +0000

    Move Prompt to beginning of loop

--- src/benchmarklib/benchmark_runner.cpp
+++ src/benchmarklib/benchmark_runner.cpp
@@ -79,6 +79,11 @@ BenchmarkRunner::BenchmarkRunner(const BenchmarkConfig& config,
 void BenchmarkRunner::run() {
 //beginning of endless loop hack
 while(true) {
+  unsigned long node;
+  std::cout << "Another Round, with a different memory policy? Please specify NUMA node." << std::endl;
+  std::cout << "> ";
+  std::cin >> node;
+  Hyrise::get().set_scheduler(std::make_shared<ImmediateExecutionScheduler>(node));
   std::cout << "- Starting Benchmark..." << std::endl;
 
   _benchmark_start = std::chrono::steady_clock::now();
@@ -181,11 +186,6 @@ while(true) {
   track_system_utilization = false;
   system_utilization_tracker.join();
 
-unsigned long node;
-std::cout << "Another Round, with a different memory policy? Please specify NUMA node." << std::endl;
-std::cout << "> ";
-std::cin >> node;
-Hyrise::get().set_scheduler(std::make_shared<ImmediateExecutionScheduler>(node));
 }
 }
 
